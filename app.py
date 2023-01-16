e1c482ecd3ec9455b0f0db194ded3d6596ba952d
bküpfer <bdokupferb@gmail.com>
2023-01-16 02:41:11 +0100

    trying bkupfer layout

--- app.py
+++ app.py
@@ -35,49 +35,6 @@ def hello_world():
     return render_template("index.html")
 
 
-@app.route('/api/options', methods=['GET', 'OPTIONS'])
-def get_options():
-    data = []
-    dynamodb = boto3.resource('dynamodb', region_name=APP_AWS_REGION)
-    table = dynamodb.Table(APP_DDB_TABLE_NAME)
-    scan_kwargs = {}
-    done = False
-    start_key = None
-    while not done:
-        if start_key:
-            scan_kwargs['ExclusiveStartKey'] = start_key
-        response = table.scan(**scan_kwargs)
-        data.extend(response.get('Items'))
-        start_key = response.get('LastEvaluatedKey', None)
-        done = start_key is None
-    return jsonify(data)
-
-
-@app.route('/api/options', methods=['POST'])
-def vote_option():
-    content = request.json
-    logging.info("Returning options")
-    logging.info("Request received: {}".format(content))
-
-    response = {}
-    if "ID" not in content:
-        response['message'] = "Malformed request"
-        return jsonify(response), 400
-
-    dynamodb = boto3.resource('dynamodb', region_name=APP_AWS_REGION)
-    table = dynamodb.Table(APP_DDB_TABLE_NAME)
-    table.update_item(
-        Key={
-            'ID': content['ID'],
-        },
-        UpdateExpression='ADD votes :inc',
-        ExpressionAttributeValues={
-            ':inc': 1
-        }
-    )
-    response['message'] = "Request processed"
-    return jsonify(response), 200
-
 if __name__ == '__main__':
     print("Hello from DevTalk")
     app.run(port=APP_PORT, host="0.0.0.0", debug=APP_DEBUG)
